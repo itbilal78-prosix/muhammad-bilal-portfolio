6dfef4fcf5ab72dcf05c186c396653cee9efa6a2
prosix-workspace <prosixsport@gmail.com>
2026-07-07 14:43:12 +0500

    Convert to Vue Vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Muhammad Bilal Portfolio</title>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
+  </head>
+  <body>
     <div id="app"></div>
-</body>
+    <script type="module" src="/resources/js/app.js"></script>
+  </body>
 </html>
